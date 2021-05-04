8e2f8ba53edc2d1add01b6f84d63a2ae441a0eff
Abraham <nencho_1@hotmail.com>
2021-05-04 19:29:57 +0200

    comment isglobal to explain it tomorrow

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -2,6 +2,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 export const DatabaseConection = [
+    //isGlobal nos permite usar la configuracion en otros modulos de las app
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_URL),
 ]
