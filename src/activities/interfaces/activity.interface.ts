5e0acaa0b544eba7180c08a0a017c66e4f5632ef
miguel <usmo300@hotmail.com>
2021-05-05 12:22:23 +0200

    created interface,dto and schema activity

---
+++ src/activities/interfaces/activity.interface.ts
@@ -0,0 +1,36 @@
+import { Document } from "mongoose"
+
+export interface Activity extends Document{
+
+    readonly title: string;
+
+    readonly price: number;
+
+    readonly img: string[];
+
+    readonly destination: string[];
+
+    readonly type: string[];
+
+    readonly sumary: string;
+
+    readonly duration: string;
+
+    readonly cancelation: string;
+    
+    readonly instant_confirmation: boolean;
+
+    readonly mobile_ticket: boolean;
+
+    readonly pet_friendly: boolean;
+    
+    readonly experience_details: string;
+    
+    readonly includes: string[];
+    
+    readonly location: string[]; 
+
+    readonly reviews: object;
+
+    readonly children: boolean;
+}
