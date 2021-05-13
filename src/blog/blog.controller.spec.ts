3386139fcce20f410591dd6d1138bdf3e454ec3c
Abraham <nencho_1@hotmail.com>
2021-05-13 14:43:07 +0200

    created test to mock all modules and functions

--- src/blog/blog.controller.spec.ts
+++ src/blog/blog.controller.spec.ts
@@ -1,13 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlogController } from './blog.controller';
+import { BlogService } from './blog.service';
 
 describe('BlogController', () => {
   let controller: BlogController;
 
+  const mockBlogService = {
+    createOneBlog: jest.fn((dto) => {
+      return {
+        ...dto
+      }
+    }),
+
+    updateOneBlog: jest.fn().mockImplementation((id, dto) => ({
+      id,
+      ...dto
+    }))
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BlogController],
-    }).compile();
+      providers: [BlogService]
+    })
+      .overrideProvider(BlogService)
+      .useValue(mockBlogService)
+      .compile();
 
     controller = module.get<BlogController>(BlogController);
   });
@@ -15,4 +33,45 @@ describe('BlogController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a blog', async() => {
+    
+    const dto = {
+      author: 'author',
+      reading_time: '14 horas',
+      title: 'title',
+      img: ['img', 'img2'],
+      description: 'description',
+      createdAt: new Date(Date.now())
+    }
+
+    expect(await controller.createOneBlog(dto)).toEqual({
+      author: dto.author,
+      reading_time: dto.reading_time,
+      title: dto.title,
+      img: dto.img,
+      description: dto.description,
+      createdAt: dto.createdAt
+    })
+
+    expect(mockBlogService.createOneBlog).toHaveBeenCalled();
+  })
+
+  it('should update a blog', async() => {
+    const dto = {
+      author: 'author',
+      reading_time: '14 horas',
+      title: 'title',
+      img: ['img', 'img2'],
+      description: 'description',
+      createdAt: new Date(Date.now())
+    }
+
+    expect(await controller.updateOneBlog(dto, '1')).toEqual({
+      id: '1',
+      ...dto
+    })
+
+    expect(mockBlogService.updateOneBlog).toHaveBeenCalled();
+  })
 });
--- src/blog/blog.service.spec.ts
+++ src/blog/blog.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlogService } from './blog.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('BlogService', () => {
   let service: BlogService;
 
+  const mockBlogModel = {
+    create: jest.fn().mockImplementation( dto => dto ),
+
+    findByIdAndUpdate: jest.fn().mockImplementation((id, dto) => ({
+      id,
+      ...dto
+    })),
+
+    findById: jest.fn().mockImplementation( id => id ),
+
+    findByIdAndDelete: jest.fn().mockImplementation( id => id ),
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BlogService],
+      providers: [BlogService, {
+        provide: getModelToken('Blog'),
+        useValue: mockBlogModel
+      }],
     }).compile();
 
     service = module.get<BlogService>(BlogService);
@@ -15,4 +32,59 @@ describe('BlogService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create a new blog', async() => {
+    
+    const dto = {
+      author: 'author',
+      reading_time: '14 horas',
+      title: 'title',
+      img: ['img', 'img2'],
+      description: 'description',
+      createdAt: new Date(Date.now())
+    }
+
+    expect(await service.createOneBlog(dto)).toEqual({
+      author: dto.author,
+      reading_time: dto.reading_time,
+      title: dto.title,
+      img: dto.img,
+      description: dto.description,
+      createdAt: dto.createdAt
+    })
+
+    expect(mockBlogModel.create).toHaveBeenCalled();
+  })
+
+  it('should update a new blog', async() => {
+    const dto = {
+      author: 'authorNew',
+      reading_time: '14 horas new',
+      title: 'title new',
+      img: ['img', 'img2'],
+      description: 'description',
+      createdAt: new Date(Date.now())
+    }
+
+    expect(await service.updateOneBlog('1', dto)).toEqual({
+      id: '1',
+      ...dto
+    })
+
+    expect(mockBlogModel.findByIdAndUpdate).toHaveBeenCalled();
+  })
+
+  it('should get an unique blog', async() => {
+    const id = '1'
+
+    expect(await service.getOneBlog(id)).toBeTruthy();
+
+    expect(mockBlogModel.findById).toHaveBeenCalled();
+  })
+
+  it('should delete a blog', async() => {
+    expect(await service.deleteOneBlog('id')).toBeTruthy();
+
+    expect(mockBlogModel.findByIdAndDelete).toHaveBeenCalled();
+  })
 });
